ecd8e2728e2e401f3730c9ddfe429a9cf9250d10
priest-tech <shikxkamanu@gmail.com>
2023-11-21 15:49:01 +0300

    last

--- regular_e/watch.py
+++ regular_e/watch.py
@@ -8,11 +8,14 @@ def main():
 
 # Extract the URL from the provided HTML
 def parse(s):
-    match= re.search(r'<iframe[^>]*\ssrc=["\'](https?://(?:www\.)?youtube\.com/embed/[^"\']+)["\']', s)
+    match = re.search(r'<iframe[^>]*\ssrc=["\'](https?://(?:www\.)?youtube\.com/embed/([^"\']+))', s)
     if match:
-        return match.group(1)
+        protocol = match.group(1)[:5]  # Extract the protocol (http or https)
+        video_id = match.group(2)  # Extract the video ID
+        return f'{protocol}{video_id}'  # Return the URL with the same protocol as provided
     else:
         return None
+
        
 
 
