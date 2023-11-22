2d683afd65f2caaad3bb5e34a52c21f16d31584f
priest-tech <shikxkamanu@gmail.com>
2023-11-22 15:09:21 +0300

    leapyear

--- Oop/seasons.py
+++ Oop/seasons.py
@@ -17,8 +17,11 @@ def get_age_in_minutes(s):
    try:
         dob = datetime.strptime(s, "%Y-%m-%d").replace(hour=0, minute=0, second=0, microsecond=0)
         today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        time = (today - dob).total_seconds() / 60
-        return num2words(time).replace(" and", "")
+        age_time = (today - dob).total_seconds() / 60
+        leap_year = today.year % 4 == 0 and (today.year % 100 != 0 or today.year % 400 == 0)
+        if leap_year:
+            age_time += 24 * 60
+        return num2words(age_time).replace(" and", "")
    except ValueError:
       return "Invalid date format"
    
